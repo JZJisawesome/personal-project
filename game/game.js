e574db2d4a63ce163c37098f3aca0f95e709a994
John Jekel <JZJisawesome123@hotmail.com>
2018-11-10 15:01:44 -0500

    moved gameSetup to game.js for less confusion

--- game/game.js
+++ game/game.js
@@ -23,6 +23,71 @@
 "use strict";
 
 /**
+ * The player's name
+ * @type {String}
+ */
+let playerName = "";
+
+/**
+ * The text to use for the game and the flow of the questions
+ * @type {Object}
+ */
+let gameText;
+
+/**
+ * The form element for getting player info
+ * @type {Element}
+ */
+const gameForm = document.getElementById("gameForm");
+
+/**
+ * The input field for getting the player's name
+ * @type {Element}
+ */
+const nameField = document.getElementById("nameField");
+
+/**
+ * The div element for displaying text
+ * @type {Element}
+ */
+const gameTextArea = document.getElementById("gameTextArea");
+
+/**
+ * The div element for buttons
+ * @type {Element}
+ */
+const gameInteractArea = document.getElementById("gameInteractArea");
+
+/**
+ * Convience function that wipes the innerHTML of gameForm, gameTextArea and gameInteractArea
+*/
+function wipeGameplayAreas()
+{
+    gameForm.innerHTML = "";
+    gameTextArea.innerHTML = "";
+    gameInteractArea.innerHTML = "";
+}
+
+/**
+ * Sets playerName based on nameField and starts the game
+*/
+function setup()
+{
+    if (nameField.value === "")
+        gameTextArea.innerHTML = "Whoops! Looks like you've forgotton to enter your name. Try again.";
+    else
+    {
+        playerName = nameField.value;
+
+        wipeGameplayAreas();
+
+        setupGameText();
+
+        intro();//in game.js
+    }
+}
+
+/**
  * Displays introduction text and a play button
 */
 function intro()
@@ -32,7 +97,6 @@ function intro()
 
     gameInteractArea.innerHTML += `<button type=\"button\" onclick=\"play()\">${gameText.intro.startButton}</button>`;//play() button
 }
-
 
 /**
  * Displays a question from gameText that the user will answer
@@ -45,4 +109,74 @@ function play(question = 0)
 
     gameInteractArea.innerHTML += gameText.questions[question].options[0];
     gameInteractArea.innerHTML += gameText.questions[question].options[1];
+}
+
+/**
+ * Creates the text for the object that is stored in gameText
+*/
+function setupGameText()
+{
+    /**
+     * Convience function to create questions for the user to answer
+     * @param {storyText} The storylinefor the question
+     * @param {firstOptionButton} The first button + tag option (options[0])
+     * @param {secondOptionButton} The second button + tag option (options[0])
+    */
+    function createQuestion(storyText, firstOptionButton, secondOptionButton)
+    {
+        return {//blame JavaScript for making me for put this brace here
+            story: storyText,
+
+            options:
+            [
+                firstOptionButton,
+                secondOptionButton
+            ]
+        };
+    }
+
+    /**
+     * Convience function to create buttons that onClick to the next question
+     * @param {nextQuestion} The question to go to if clicked (parameter of play())
+     * @param {buttonText} The text on the button
+    */
+    function createOptionButtonTag(nextQuestion, buttonText)
+    {
+        if (nextQuestion === undefined || buttonText === undefined)
+            return "";
+        else
+            return `<button type=\"button\" onclick=\"play(${nextQuestion})\">${buttonText}</button>`;
+    }
+
+    gameText =
+    {
+        intro:
+        {
+            greeting:       `Placeholder greeting text (name: ${playerName})`,
+            introText:      "Placeholder introduction text",
+            startButton:    "Placeholder startButton text",
+        },
+
+        outro:
+        {
+
+        },
+
+        questions://an array of storylines and decisions
+        [
+            createQuestion
+            (
+                "Placeholder story text (story: 0)",
+                createOptionButtonTag(1, "Placeholder option text (option: 0)"),
+                createOptionButtonTag(1, "Placeholder option text (option: 1)")
+            ),
+
+            createQuestion
+            (
+                "Placeholder story text (story: 1)",
+                createOptionButtonTag(0, "Placeholder option text (option: 0)"),
+                createOptionButtonTag(0, "Placeholder option text (option: 1)")
+            ),
+        ]
+    };
 }
